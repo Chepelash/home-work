b75b7892aa68468489f66d844e166d180f6c5940
Anton Chepelev <chepelash@gmail.com>
2021-11-13 20:08:12 +0300

    Выполнено основное задание

--- src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
+++ src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
@@ -1,16 +1,76 @@
 package com.sbrf.reboot.repository;
 
 import com.sbrf.reboot.dto.Account;
+import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @AllArgsConstructor
 public class AccountRepositoryImpl implements AccountRepository {
     private String fpath;
+
+    private Map<Long, Set<Account>> parseFile(@NotNull String fpath) throws FileNotFoundException {
+
+        Map<Long, Set<Account>> map = new HashMap();  // dictionary <key = clientId, value = Set<Account>>
+
+        // FileInputStream(file) throws FileNotFoundException if the file does not exist,
+        // is a directory rather than a regular file, or for some other reason cannot be opened for reading
+        StringBuilder sb = new StringBuilder();
+        InputStream inputStream = new FileInputStream(fpath);
+        try{
+            // read file into string
+            int content;
+            while((content = inputStream.read()) != -1)
+                sb.append((char) content);
+        } catch (IOException e){
+            return null;
+        }
+        String fileContent = sb.toString();
+        // parse string and fill map
+        String[] entries = fileContent.split("},");
+        Pattern pattern = Pattern.compile("\"clientId\":\\s?(?<clientId>\\d+),\\s+\"number\":\\s?\"(?<number>\\S+)\"");
+        Matcher matcher;
+        for(String entry: entries){
+            matcher = pattern.matcher(entry);
+            if(matcher.find()){
+                Long clientId = Long.parseLong(matcher.group("clientId"));
+                String number = matcher.group("number");
+                if(map.containsKey(clientId)){
+                    // clientId exists
+                    // get old value
+                    Set<Account> accounts = map.get(clientId);
+                    // add new number
+                    accounts.add(new Account(number));
+                    // replace
+                    map.replace(clientId, accounts);
+                } else {
+                    // add new cliendId
+                    Set<Account> accounts = new HashSet();
+                    accounts.add(new Account(number));
+                    map.put(clientId, accounts);
+                }
+            }
+        }
+
+        return map;
+    }
+
     @Override
     public Set<Account> getAllAccountsByClientId(long id) throws FileNotFoundException {
-        return null;
+        // parse file
+        Map<Long, Set<Account>> accountMap = parseFile(this.fpath);
+
+        // get set<account> and return it
+        if(accountMap == null)
+            return null;
+        return accountMap.get(id);
     }
 }

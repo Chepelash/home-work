55c58f2eb3d438a98ff30e06e4ccaab0cb77eb3a
Anton Chepelev <chepelash@gmail.com>
2021-11-14 14:22:26 +0300

    Написаны тесты на новый метод

--- src/test/java/com/sbrf/reboot/repository/AccountRepositoryImplTest.java
+++ src/test/java/com/sbrf/reboot/repository/AccountRepositoryImplTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -46,5 +50,39 @@ class AccountRepositoryImplTest {
         });
     }
 
+    @Test
+    void successUpdateValue() throws IOException, NoSuchFieldException {
+        // copy test file
+        Path copied = Paths.get("src/main/resources/Accounts_copy.txt");
+        Path originalPath = Paths.get("src/main/resources/Accounts.txt");
+        Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
+        accountRepository = new AccountRepositoryImpl(copied.toString());
+        accountRepository.updateClientNumber(3L, "3-ACCNUM", "5-ACCNUM");
+        Set<Account> allAccountsByClientId = accountRepository.getAllAccountsByClientId(3L);
+        assertEquals(1, (int) allAccountsByClientId.stream().filter(e -> e.getNumber().equals("5-ACCNUM")).count());
+    }
 
+    @Test
+    void fileNotExistsUpdateValue() {
+        accountRepository = new AccountRepositoryImpl("somePath");
+        assertThrows(FileNotFoundException.class, () -> {
+            accountRepository.updateClientNumber(1L, "3-ACCNUM", "5-ACCNUM");
+        });
+    }
+
+    @Test
+    void idNotExistsUpdateValue() {
+        accountRepository = new AccountRepositoryImpl("src/main/resources/Accounts.txt");
+        assertThrows(NoSuchFieldException.class, () -> {
+            accountRepository.updateClientNumber(4L, "", "");
+        });
+    }
+
+    @Test
+    void numberNotExistsUpdateValue() {
+        accountRepository = new AccountRepositoryImpl("src/main/resources/Accounts.txt");
+        assertThrows(NoSuchFieldException.class, () -> {
+            accountRepository.updateClientNumber(1L, "213123", "5-ACCNUM");
+        });
+    }
 }

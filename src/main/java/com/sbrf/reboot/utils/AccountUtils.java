1536b644586c976cdf3204595281bcbc9951dda1
Anton Chepelev <chepelash@gmail.com>
2021-11-26 21:32:05 +0300

    Написан класс AccountUtils и тесты на все методы

--- src/main/java/com/sbrf/reboot/utils/AccountUtils.java
+++ src/main/java/com/sbrf/reboot/utils/AccountUtils.java
@@ -2,13 +2,24 @@ package com.sbrf.reboot.utils;
 
 import com.sbrf.reboot.dto.Account;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class AccountUtils {
 
     public static void sortedById(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId));
     }
 
     public static void sortedByIdDate(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId)
+                      .thenComparing(Account::getCreateDate));
+    }
+
+    public static void sortedByIdDateBalance(List<Account> accounts) {
+        accounts.sort(Comparator.comparing(Account::getId)
+                .thenComparing(Account::getCreateDate)
+                .thenComparing(Account::getBalance));
     }
 }
--- src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
+++ src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
@@ -46,4 +46,31 @@ class AccountUtilsTest {
         Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
         Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
     }
+
+    @Test
+    void sortedByIdDateBalance() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().id(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.valueOf(6)).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.valueOf(7)).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.valueOf(6)).build());
+        }};
+
+        AccountUtils.sortedByIdDateBalance(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getId());
+        Assertions.assertEquals(2L, accounts.get(1).getId());
+        Assertions.assertEquals(2L, accounts.get(2).getId());
+        Assertions.assertEquals(2L, accounts.get(3).getId());
+
+        Assertions.assertEquals(LocalDate.now().minusDays(4), accounts.get(0).getCreateDate());
+        Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(1).getCreateDate());
+        Assertions.assertEquals(LocalDate.now(), accounts.get(2).getCreateDate());
+        Assertions.assertEquals(LocalDate.now(), accounts.get(3).getCreateDate());
+
+        Assertions.assertEquals(BigDecimal.TEN, accounts.get(0).getBalance());
+        Assertions.assertEquals(BigDecimal.valueOf(6), accounts.get(1).getBalance());
+        Assertions.assertEquals(BigDecimal.valueOf(6), accounts.get(2).getBalance());
+        Assertions.assertEquals(BigDecimal.valueOf(7), accounts.get(3).getBalance());
+    }
 }

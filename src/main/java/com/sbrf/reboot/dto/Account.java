6143aa5b0490b251aa659bca1f786900f4adbe3b
Anton Chepelev <chepelash@gmail.com>
2021-11-26 21:04:46 +0300

    Обновлен класс Account: новые поля, lombok builder

--- src/main/java/com/sbrf/reboot/dto/Account.java
+++ src/main/java/com/sbrf/reboot/dto/Account.java
@@ -1,12 +1,19 @@
 package com.sbrf.reboot.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Account {
-    private String number;
+    private long id;
+    private LocalDate createDate;
+    private BigDecimal balance;
 }

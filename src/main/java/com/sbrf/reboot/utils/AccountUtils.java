01fba294b82602d129f1bf79a6b5c8573f8b04c6
Anton Chepelev <chepelash@gmail.com>
2021-11-26 21:05:35 +0300

    Создан класс AccountUtils

---
+++ src/main/java/com/sbrf/reboot/utils/AccountUtils.java
@@ -0,0 +1,14 @@
+package com.sbrf.reboot.utils;
+
+import com.sbrf.reboot.dto.Account;
+
+import java.util.List;
+
+public class AccountUtils {
+
+    public static void sortedById(List<Account> accounts) {
+    }
+
+    public static void sortedByIdDate(List<Account> accounts) {
+    }
+}
